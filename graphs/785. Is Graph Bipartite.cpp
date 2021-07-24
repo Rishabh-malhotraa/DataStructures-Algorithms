2c51147c694c02ce97ccd6da878193fc21a37f45
Rishabh Malhotra <Rishabhmalhotraa01@gmail.com>
2021-07-24 18:22:46 +0530

    graphs 😨😱

---
+++ graphs/785. Is Graph Bipartite.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// BFS method of finding bipartite but coloring leads to a better looking code
+
+class Solution
+{
+public:
+  bool isBipartiteUtil(vector<vector<int>> &graph, vector<bool> &visited, vector<int> &color, int root)
+  {
+    bool result = true;
+    queue<int> q;
+    q.push(root);
+    visited[root] = true;
+    int offset = 0;
+    while (!q.empty())
+    {
+      int size = q.size();
+      for (int i = 0; i < size; i++)
+      {
+        int curr = q.front();
+
+        visited[curr] = true;
+
+        if (color[curr] == (offset + 1) % 2)
+          return false;
+        else
+          color[curr] = offset % 2;
+
+        for (int neighbour : graph[curr])
+        {
+          if (visited[neighbour] == false)
+            q.push(neighbour);
+        }
+        q.pop();
+      }
+      offset++;
+    }
+
+    return result;
+  }
+
+  bool isBipartite(vector<vector<int>> &graph)
+  {
+    int n = graph.size(), m = graph[0].size();
+    bool result = false;
+    vector<bool> visited(n, false);
+    vector<int> color(n, -1);
+
+    for (int i = 0; i < n; i++)
+    {
+      if (visited[i] == false)
+        result = isBipartiteUtil(graph, visited, color, i);
+
+      if (result == false)
+        break;
+    }
+    return result;
+  }
+};
---
+++ graphs/886 - Possible Bipartition.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// DFS Methof of coloring the graph
+
+class Solution
+{
+private:
+  enum States
+  {
+    WHITE,
+    GREY,
+    BLACK
+  };
+
+  bool dfs(vector<list<int>> &graph, vector<int> &state, int node, int color)
+  {
+    if (state[node] != WHITE)
+      return color == state[node];
+
+    state[node] = color;
+    int nextColor = (color == BLACK) ? GREY : BLACK;
+    for (int neighbour : graph[node])
+    {
+      bool result = dfs(graph, state, neighbour, nextColor);
+      if (result == false)
+        return false;
+    }
+    return true;
+  }
+
+public:
+  bool possibleBipartition(int n, vector<vector<int>> &dislikes)
+  {
+    vector<int> state(n + 1, WHITE);
+    vector<list<int>> graph(n + 1);
+    bool result = true;
+    for (vector<int> &edges : dislikes)
+    {
+      graph[edges[0]].push_back(edges[1]);
+      graph[edges[1]].push_back(edges[0]);
+    }
+
+    for (int i = 1; i < n + 1; i++)
+    {
+      if (state[i] == WHITE)
+      {
+        result = dfs(graph, state, i, BLACK);
+        if (result == false)
+          return result;
+      }
+    }
+    return result;
+  }
+};
